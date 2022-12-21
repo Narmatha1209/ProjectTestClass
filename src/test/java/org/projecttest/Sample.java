95e3837ab2b28499a3a57950d0dad5559e603eb4
Narmatha1209 <narmatha.bca3@gmail.com>
2022-12-21 11:06:57 +0530

    Nimmi's code

--- src/test/java/org/projecttest/Sample.java
+++ src/test/java/org/projecttest/Sample.java
@@ -5,7 +5,8 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test1");
-
+		System.out.println("test4");
+		System.out.println("test5");
 	}
 
 }
